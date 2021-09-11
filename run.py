be4412271e226898f8e21252446a48dddee7871f
HeleJ <hele.juhanson@gmail.com>
2021-09-11 22:11:57 +0000

    Add display art and greetings

--- run.py
+++ run.py
@@ -7,8 +7,29 @@ weather = ["climate", "isobar", "visibility",
 irish_names = ["caoimhe", "saoirse", "clodagh",
                "roisin", "eireann", "padraig"]
 counties_of_ireland = ["limerick", "tipperary", "wexford",
-                       "donegal","longford", "galway"]               
+                       "donegal", "longford", "galway"]   
 
+category = [weather, irish_names, counties_of_ireland]    
+
+
+def display_greeting():
+    """
+    Prompt user to input name and greetings
+    """
+    print("WELCOME TO")
+    # Title ASCII ART 
+    print(" ▄  █ ██      ▄     ▄▀  █▀▄▀█ ██      ▄")       
+    print("█   █ █ █      █  ▄▀    █ █ █ █ █      █")      
+    print("██▀▀█ █▄▄█ ██   █ █ ▀▄  █ ▄ █ █▄▄█ ██   █")     
+    print("█   █ █  █ █ █  █ █   █ █   █ █  █ █ █  █")    
+    print("   █     █ █  █ █  ███     █     █ █  █ █")     
+    print("  ▀     █  █   ██         ▀     █  █   ██")    
+    print("       ▀                       ▀")
+    name = input("Enter your name:")
+    print("Hello" + name + "Best of luck!")
+    time.sleep(2)
+    print("The game is about to start!\n Let's play Hangman!")              
+      
 
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
