55d174a5b13aa338afa4dcedcf7ae6935fea0767
HeleJ <hele.juhanson@gmail.com>
2021-09-11 21:18:27 +0000

    Add list for questions

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
+import random
+import time
+
+# List for guestion word
+weather = ["climate", "isobar", "visibility",
+           "showery", "unsettled", "reinbow"]
+irish_names = ["caoimhe", "saoirse", "clodagh",
+               "roisin", "eireann", "padraig"]
+counties_of_ireland = ["limerick", "tipperary", "wexford",
+                       "donegal","longford", "galway"]               
+
+
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
